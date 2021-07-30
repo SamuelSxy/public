c89a6cf03d5cdd45e8b422a501e3af0530db08c9
SamuelSxy <samuel.900215@gmail.com>
2021-07-30 20:03:09 +0800

    Add files via upload
    [Add Feature]极客时间 go训练营 第二周作业

---
+++ main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/pkg/errors"
+	"golang.org/x/sync/errgroup"
+)
+
+func main() {
+	g, ctx := errgroup.WithContext(context.Background())
+
+	sigChan := make(chan os.Signal, 1)
+	signal.Notify(sigChan, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT, syscall.SIGSTOP)
+
+	serv := http.Server{Addr: ":8080"}
+	http.HandleFunc("/", helloWord)
+
+	g.Go(func() error {
+		go func() {
+			<-ctx.Done()
+			serv.Shutdown(context.TODO())
+		}()
+		return serv.ListenAndServe()
+	})
+
+	g.Go(func() error {
+		select {
+		case <-ctx.Done():
+			return ctx.Err()
+		case s := <-sigChan:
+			return errors.Errorf("signal: %v", s)
+		}
+
+		return nil
+	})
+
+	if err := g.Wait(); err != nil {
+		fmt.Println(err)
+	}
+
+}
+
+func helloWord(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello golang http!")
+}
